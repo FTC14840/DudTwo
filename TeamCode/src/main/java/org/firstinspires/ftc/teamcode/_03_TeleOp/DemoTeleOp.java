13d017f93ff57f2b65f571a926396460add5dc6e
FTC <ftc14840@gmail.com>
2026-03-26 16:43:02 -0500

    added demo TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_03_TeleOp/DemoTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_03_TeleOp/DemoTeleOp.java
@@ -2,18 +2,48 @@ package org.firstinspires.ftc.teamcode._03_TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="demo TeleOp")
 public class DemoTeleOp extends LinearOpMode {
-    public Servo testServo;
+    public DcMotor testMotor;
+    public CRServo testServo;
     @Override
     public void runOpMode() throws InterruptedException {
+        initMotor();
+        initServo();
+        waitForStart();
+        while(opModeIsActive()){
+            if(gamepad1.a){
+                testMotor.setPower(0);
 
+            }
+            if(gamepad1.y) {
+                testServo.setPower(0);
+            }
+            if(gamepad1.b){
+                testMotor.setPower(1);
+            }
+            if(gamepad1.x) {
+                testServo.setPower(1);
+            }
+            }
+        }
+
+    public void initMotor() {
+        testMotor = this.hardwareMap.get(DcMotor.class, "testMotor");
+        testMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        testMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        testMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        testMotor.setPower(0);
     }
     public void initServo() {
-        testServo = this.hardwareMap.get(Servo.class, "testServo");
-        testServo.setDirection(Servo.Direction.FORWARD);
-        testServo.setPosition(0);
+        testServo = this.hardwareMap.get(CRServo.class, "testServo");
+        testServo.setDirection(CRServo.Direction.FORWARD);
+        testServo.setPower(0);
     }
 }
+
